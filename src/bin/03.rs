0787997270ecd290905f2bdaa58a264f2863e717
Brett Norris <b.norris@calendly.com>
2022-12-05 00:43:06 -0500

    formt

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -5,7 +5,7 @@ const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 struct Backpack {
     items: HashSet<char>,
-    compartments: Vec<HashSet<char>>
+    compartments: Vec<HashSet<char>>,
 }
 
 impl Backpack {
@@ -17,32 +17,39 @@ impl Backpack {
 
         Backpack {
             items: input.chars().into_iter().collect::<HashSet<char>>(),
-            compartments: vec![
-                first_compartment,
-                second_compartment
-            ]
+            compartments: vec![first_compartment, second_compartment],
         }
     }
 
     fn priority(&self) -> Option<u32> {
         let dupes = self.find_duplicates();
-        Some(dupes.iter().map(|char| score_item(&char).unwrap() as u32).sum())
+        Some(
+            dupes
+                .iter()
+                .map(|char| score_item(&char).unwrap() as u32)
+                .sum(),
+        )
     }
 
     fn find_duplicates(&self) -> Vec<char> {
         let first = &self.compartments[0];
         let second = &self.compartments[1];
-        first.intersection(second).map(|i| *i).collect::<Vec<char>>()
+        first
+            .intersection(second)
+            .map(|i| *i)
+            .collect::<Vec<char>>()
     }
 }
 
 fn find_duplicates(backpacks: Vec<Backpack>) -> Vec<char> {
-    let dupes = backpacks
-        .iter()
-        .skip(1)
-        .fold(backpacks[0].items.clone(), |acc: HashSet<char>, backpack| {
-            acc.intersection(&backpack.items.clone()).cloned().collect::<HashSet<char>>()
-        });
+    let dupes = backpacks.iter().skip(1).fold(
+        backpacks[0].items.clone(),
+        |acc: HashSet<char>, backpack| {
+            acc.intersection(&backpack.items.clone())
+                .cloned()
+                .collect::<HashSet<char>>()
+        },
+    );
 
     Vec::from_iter(dupes)
 }
@@ -50,16 +57,14 @@ fn find_duplicates(backpacks: Vec<Backpack>) -> Vec<char> {
 fn score_item(item: &char) -> Option<u8> {
     match ALPHABET.chars().position(|c| &c == item) {
         Some(i) => Some((i + 1) as u8),
-        None => None
+        None => None,
     }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     input
         .lines()
-        .map(|line| {
-            Backpack::from_string(line).priority()
-        })
+        .map(|line| Backpack::from_string(line).priority())
         .sum()
 }
 
@@ -71,9 +76,7 @@ pub fn part_two(input: &str) -> Option<u32> {
         .map(|chunk| {
             let backpacks = chunk
                 .iter()
-                .map(|line| {
-                    Backpack::from_string(line)
-                })
+                .map(|line| Backpack::from_string(line))
                 .collect::<Vec<Backpack>>();
 
             find_duplicates(backpacks)
