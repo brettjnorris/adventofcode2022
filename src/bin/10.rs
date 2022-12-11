0e6b94c7b9f042cd307631e5177b9da03c3fadba
Brett Norris <b.norris@calendly.com>
2022-12-11 00:40:26 -0500

    Day 10

---
+++ src/bin/10.rs
@@ -0,0 +1,158 @@
+use std::ops::Range;
+
+#[derive(Debug)]
+enum Instruction {
+    Add(i32),
+    Noop
+}
+
+fn parse_instruction(input: &str) -> Instruction {
+    let parts = input.split_whitespace().collect::<Vec<&str>>();
+    match parts[0] {
+        "addx" => {
+            Instruction::Add(parts[1].parse::<i32>().unwrap())
+        },
+        _ => Instruction::Noop
+    }
+}
+
+struct CPU {
+    counter: u32,
+    register_x: i32,
+    delay: u32,
+    instructions: Vec<Instruction>,
+    next_instruction: Option<Instruction>
+}
+
+impl CPU {
+    fn from_input(input: &str) -> CPU {
+        let instructions = input
+            .lines()
+            .map(|line| {
+                parse_instruction(line)
+            })
+            .rev()
+            .collect::<Vec<Instruction>>();
+
+        println!("{:?}", &instructions);
+
+        CPU {
+            instructions,
+            next_instruction: None,
+            counter: 0,
+            register_x: 1,
+            delay: 0,
+        }
+    }
+
+    fn tick(&mut self) {
+        self.counter += 1;
+
+        // Apply last instruction
+        if self.delay == 0 {
+            if let Some(Instruction::Add(val)) =  self.next_instruction {
+                 self.register_x += val;
+            }
+
+            let instruction = self.instructions.pop();
+
+            if let Some(Instruction::Add(_)) = instruction {
+                self.delay = 1;
+            }
+            self.next_instruction = instruction;
+        } else {
+            self.delay -= 1;
+        }
+    }
+
+    fn draw_pixel(&self) -> bool {
+        let range = ((self.register_x - 1)..=(self.register_x + 1));
+
+        let contains = range.contains(&((self.counter - 1) as i32 % 40));
+        println!("{:?}, {}, contains: {}", range, self.counter, contains);
+
+        contains
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut cpu = CPU::from_input(input);
+    let mut signal_strength = 0;
+
+    loop {
+        cpu.tick();
+        println!("counter: {:?}, delay: {:?}, register_x: {:?}, next_instruction: {:?}", cpu.counter, cpu.delay, cpu.register_x, cpu.next_instruction);
+
+        match cpu.counter {
+            20 | 60 | 100 | 140 | 180 | 220 => {
+                let score = cpu.counter as i32 * cpu.register_x;
+                println!("score: {}", score);
+                signal_strength += score;
+            },
+            _ => ()
+        }
+
+        if cpu.next_instruction.is_none() {
+            break;
+        }
+    }
+    Some(signal_strength as u32)
+}
+
+pub fn part_two(input: &str) -> Option<String> {
+    let mut cpu = CPU::from_input(input);
+    let mut output: Vec<&str> = vec![];
+
+    loop {
+        cpu.tick();
+
+        println!("counter: {:?}, delay: {:?}, register_x: {:?}, next_instruction: {:?}", cpu.counter, cpu.delay, cpu.register_x, cpu.next_instruction);
+
+        if cpu.draw_pixel() {
+            output.push("#");
+        } else {
+            output.push(".");
+        }
+
+        println!("{:?}", output.join(""));
+
+        if cpu.next_instruction.is_none() {
+            break;
+        }
+    }
+
+    let printed = output
+        .chunks(40)
+        .map(|chunk| {
+            chunk.join("")
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    println!("{}", printed);
+    Some(printed)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 10);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 10);
+        assert_eq!(part_one(&input), Some(13140));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 10);
+        let expected_output = "##..##..##..##..##..##..##..##..##..##..\n###...###...###...###...###...###...###.\n####....####....####....####....####....\n#####.....#####.....#####.....#####.....\n######......######......######......####\n#######.......#######.......#######.....\n.".to_string();
+        assert_eq!(part_two(&input), Some(expected_output));
+    }
+}
